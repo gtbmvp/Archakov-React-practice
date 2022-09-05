a62bce23d1c38ceaeafdf68a4604da383396d188
grindingeorge <grindingeorge@gmail.com>
2022-09-05 11:26:31 +0300

    tutorial-3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // import App from "./tutorial-1/App";
-import App from "./tutorial-2/App";
+// import App from "./tutorial-2/App";
+import App from "./tutorial-3/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
