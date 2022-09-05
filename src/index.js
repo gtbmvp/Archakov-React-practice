b283e7165753b79db96fcf1793b0c5b831c357d8
grindingeorge <grindingeorge@gmail.com>
2022-09-05 17:40:26 +0300

    Generate random phrase

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 
 // import App from "./tutorial-1/App";
 // import App from "./tutorial-2/App";
-import App from "./tutorial-3/App";
+// import App from "./tutorial-3/App";
+import App from "./tutorial-4 (generate)/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
